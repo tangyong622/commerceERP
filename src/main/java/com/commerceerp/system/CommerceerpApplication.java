c347e3d319c65aaf3dfe85eb8fff7a5972b03f17
tangyong <tangyong622@163.com>
2018-06-05 13:55:12 +0800

    提交空白项目

---
+++ src/main/java/com/commerceerp/system/CommerceerpApplication.java
@@ -0,0 +1,12 @@
+package com.commerceerp.system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommerceerpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommerceerpApplication.class, args);
+	}
+}
---
+++ src/test/java/com/commerceerp/system/CommerceerpApplicationTests.java
@@ -0,0 +1,16 @@
+package com.commerceerp.system;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CommerceerpApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
